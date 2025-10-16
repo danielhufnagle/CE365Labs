1c532d0c5968915fadbd0933bbf5009c3bcacbad
Daniel Hufnagle <danielchufnagle@gmail.com>
2025-10-15 19:30:20 -0500

    updated MPU_SPI

--- MPU_SPI.ino
+++ MPU_SPI.ino
@@ -1,44 +1,103 @@
-#include <SPI.h>
-#include "MPU6050.h"
+/***************************************************************************
+* Example sketch for the MPU6500_WE library
+*
+* This sketch shows how to get acceleration, gyroscocope and temperature 
+* data from the MPU6500 using SPI.
+* 
+* For further information visit my blog:
+*
+* https://wolles-elektronikkiste.de/mpu9250-9-achsen-sensormodul-teil-1  (German)
+* https://wolles-elektronikkiste.de/en/mpu9250-9-axis-sensor-module-part-1  (English)
+* 
+***************************************************************************/
 
-#define CS_PIN 10
+#include <MPU6500_WE.h>
+const int csPin = 10;  // Chip Select Pin
+// const int mosiPin = 22;  // "MOSI" Pin
+// const int misoPin = 21;  // "MISO" Pin
+// const int sckPin = 16;  // SCK Pin
+bool useSPI = true;    // SPI use flag
 
-// Create MPU6050 object in SPI mode
-MPU6050 mpu(CS_PIN);  
+
+/* There are two constructors for SPI: */
+MPU6500_WE myMPU6500 = MPU6500_WE(&SPI, csPin, useSPI);
+
+/* Use this one if you want to change the default SPI pins (only for ESP32 / STM32 so far): */
+// MPU6500_WE myMPU6500 = MPU6500_WE(&SPI, csPin, mosiPin, misoPin, sckPin, useSPI);
+
+/* Changing SPI pins on STM32 boards can be a bit diffcult - the following worked on a Nucleo-L432KC board:
+
+    const int csPin = D3;   
+    const int mosiPin = A6; 
+    const int misoPin = D10; 
+    const int sckPin = A1;
+    bool useSPI = true;    // SPI use flag
+    MPU6500_WE myMPU6500 = MPU6500_WE(&SPI, csPin, mosiPin, misoPin, sckPin, useSPI);
+
+   Or, using the same pins:
+    SPIClass mySPI(mosiPin, misoPin, sckPin); // don't pass the CS-Pin (=SSEL)
+    MPU6500_WE myMPU6500 = MPU6500_WE(&mySPI, csPin, useSPI);
+*/
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial);
-
-  // Initialize SPI
-  SPI.begin();
-
-  Serial.println("Initializing MPU-6500 over SPI...");
-  mpu.initialize_SPI(); // Use SPI init version
-
-  // Check connection
-  if (mpu.testConnection()) {
-    Serial.println("MPU-6500 connection successful!");
-  } else {
-    Serial.println("MPU-6500 connection failed!");
-    while (1);
+  Wire.begin();
+  if(!myMPU6500.init()){
+    Serial.println("MPU6500 does not respond");
   }
+  else{
+    Serial.println("MPU6500 is connected");
+  }
+
+  /* Choose the SPI clock speed, default is 8 MHz 
+     This function must be used only after init(), not before */
+  //myMPU9250.setSPIClockSpeed(4000000);
+
+  Serial.println("Position you MPU6500 flat and don't move it - calibrating...");
+  delay(1000);
+  myMPU6500.autoOffsets();
+  Serial.println("Done!");
+    
+  //myMPU6500.setAccOffsets(-14240.0, 18220.0, -17280.0, 15590.0, -20930.0, 12080.0);
+  //myMPU6500.setGyrOffsets(45.0, 145.0, -105.0);
+  myMPU6500.enableGyrDLPF();
+  //myMPU6500.disableGyrDLPF(MPU6500_BW_WO_DLPF_8800); // bandwdith without DLPF
+  myMPU6500.setGyrDLPF(MPU6500_DLPF_6);
+  myMPU6500.setSampleRateDivider(5);
+  myMPU6500.setGyrRange(MPU6500_GYRO_RANGE_250);
+  myMPU6500.setAccRange(MPU6500_ACC_RANGE_2G);
+  myMPU6500.enableAccDLPF(true);
+  myMPU6500.setAccDLPF(MPU6500_DLPF_6);
+  //myMPU6500.enableAccAxes(MPU6500_ENABLE_XYZ);
+  //myMPU6500.enableGyrAxes(MPU6500_ENABLE_XYZ);
 }
 
 void loop() {
-  int16_t ax, ay, az, gx, gy, gz;
-  
-  // Read raw accel and gyro
-  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  xyzFloat gValue = myMPU6500.getGValues();
+  xyzFloat gyr = myMPU6500.getGyrValues();
+  float temp = myMPU6500.getTemperature();
+  float resultantG = myMPU6500.getResultantG(gValue);
 
-  Serial.print("a/g:\t");
-  Serial.print(ax); Serial.print("\t");
-  Serial.print(ay); Serial.print("\t");
-  Serial.print(az); Serial.print("\t");
-  Serial.print(gx); Serial.print("\t");
-  Serial.print(gy); Serial.print("\t");
-  Serial.println(gz);
+  Serial.println("Acceleration in g (x,y,z):");
+  Serial.print(gValue.x);
+  Serial.print("   ");
+  Serial.print(gValue.y);
+  Serial.print("   ");
+  Serial.println(gValue.z);
+  Serial.print("Resultant g: ");
+  Serial.println(resultantG);
 
-  delay(200);
+  Serial.println("Gyroscope data in degrees/s: ");
+  Serial.print(gyr.x);
+  Serial.print("   ");
+  Serial.print(gyr.y);
+  Serial.print("   ");
+  Serial.println(gyr.z);
+
+  Serial.print("Temperature in °C: ");
+  Serial.println(temp);
+
+  Serial.println("********************************************");
+
+  delay(1000);
 }
-
